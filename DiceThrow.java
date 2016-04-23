a5954b39b9e24adc77efb00ff8b900072f636b2b
Dainik Tandel <dainiktandel@gmail.com>
2016-04-23 17:39:55 +0530

    Delete DiceThrow.java

--- DiceThrow.java
+++
@@ -1,8 +0,0 @@
-public class DiceThrow 
-{
-	public static int getDiceThrowValue(int diceThrow)
-	{
-		diceThrow = (int)(Math.random() * 6) + 1;			// Generate Random number from 1 to 6 and store in diceThrow array variable.
-		return diceThrow;
-	}
-}
